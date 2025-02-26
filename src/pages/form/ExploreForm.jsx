39bef8c7e8fb7bc004184bf63867916e53d927ea
rayanahmed <thismac@rayanahmed.Dlink>
2025-02-26 17:14:28 +0600

    basic error handling done with formhook with zod

--- src/pages/form/ExploreForm.jsx
+++ src/pages/form/ExploreForm.jsx
@@ -1,15 +1,10 @@
 import { Button, Checkbox, Error, Input } from "../../components/UI";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z as zod } from "zod";
 import { registrationSchema } from "../../utils/schema";
 
 const ExploreForm = () => {
-   const {
-      register,
-      handleSubmit,
-      formState: { errors },
-   } = useForm({ resolver: zodResolver(registrationSchema) });
+   const { register, handleSubmit, formState } = useForm({ resolver: zodResolver(registrationSchema) });
 
    const onSubmit = (data) => {
       console.log(data);
@@ -21,15 +16,15 @@ const ExploreForm = () => {
             <h1>Registration </h1>
             <form onSubmit={handleSubmit(onSubmit)} action="" className="space-y-3 mt-5">
                <Input {...register("name")} type="text" placeholder="Enter your name" />
-               {errors.name && <Error text={errors.name.message} />}
+               {formState.errors.name && <Error text={formState.errors.name.message} />}
                <Input {...register("email")} type="email" placeholder="Enter your email" />
-               {errors.email && <Error text={errors.email.message} />}
+               {formState.errors.email && <Error text={formState.errors.email.message} />}
                <Input {...register("password")} type="text" placeholder="Enter your password" />
-               {errors.password && <Error text={errors.password.message} />}
+               {formState.errors.password && <Error text={formState.errors.password.message} />}
                <Input {...register("confirmPassword")} type="text" placeholder="Confirm your password" />
-               {errors.confirmPassword && <Error text={errors.confirmPassword.message} />}
+               {formState.errors.confirmPassword && <Error text={formState.errors.confirmPassword.message} />}
                <Checkbox {...register("terms")} title="Agree to terms and conditions" />
-               {errors.terms && <Error text={errors.terms.message} />}
+               {formState.errors.terms && <Error text={formState.errors.terms.message} />}
                <Button type="submit">Submit</Button>
             </form>
          </div>
