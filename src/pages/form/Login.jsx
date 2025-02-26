3bb2e6bd7b583f52bf35ac3ebc27652d9df7c336
rayanahmed <thismac@rayanahmed.Dlink>
2025-02-26 23:32:52 +0600

    login functionality added

--- src/pages/form/Login.jsx
+++ src/pages/form/Login.jsx
@@ -2,12 +2,19 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchemas } from "../../utils/schema";
 import { Button, Error, Input } from "../../components/UI";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.config";
 
 const Login = () => {
    const { register, handleSubmit, formState } = useForm({ resolver: zodResolver(loginSchemas) });
 
-   const submitData = (data) => {
-      console.log(data);
+   const submitData = async (data) => {
+      try {
+         const user = await signInWithEmailAndPassword(auth, data.email, data.password);
+         console.log(user);
+      } catch (error) {
+         console.log(error);
+      }
    };
 
    return (
